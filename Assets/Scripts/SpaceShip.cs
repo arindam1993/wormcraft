9b47ab2f6c7b82006a7e3e4432977c6c55d775ab
wisesm0 <dyadicofficial@gmail.com>
2020-02-02 12:52:14 -0800

    Fix spaceship stuff

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class SpaceShip : MonoBehaviour
 {
 
+    public ParticleSystem smoke;
     private float PercentageFixed = 0;
+    private ParticleSystem.MainModule smoke_main;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        smoke = this.GetComponent<ParticleSystem>();
+        smoke_main = smoke.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tools"))
+        {
+            this.addToFixed();
+        }
     }
 
     public void addToFixed()
     {
-        PercentageFixed += 5;
+        if (PercentageFixed <= 100)
+        {
+            PercentageFixed += 5;
+        }
+        smoke_main.startSize = smoke_main.startSize.constant - 0.1f;
     }
 
     public float GetPercentFixed()
