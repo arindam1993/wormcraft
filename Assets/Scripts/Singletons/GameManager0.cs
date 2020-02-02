77e83ce15a2ee784f44f91674d5ddd85cd88ee2b
wisesm0 <dyadicofficial@gmail.com>
2020-02-02 12:31:38 -0800

    Add spaceship prefab

---
+++ Assets/Scripts/Singletons/GameManager0.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager0 : MonoBehaviour
+{
+    public static GameManager0 Instance = null;
+    public SpaceShip spaceship;
+    public GameObject end;
+
+    public TextMeshProUGUI distanceText;
+    public TextMeshProUGUI percentFixed;
+
+    private string DISTANCE = "Distance";
+    private string PERCENTAGE = "Percentage Fixed : ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        distanceText.text = DISTANCE + " : " + this.calculateDistance() + " m";
+        percentFixed.text = PERCENTAGE + this.GetPercentageFixed().ToString() + " %";
+    }
+
+    private string calculateDistance()
+    {
+        float dist = Vector3.Distance(spaceship.transform.position, end.transform.position);
+        return dist.ToString();
+    }
+
+    public float GetPercentageFixed()
+    {
+        return spaceship.GetPercentFixed();
+    }
+
+}
