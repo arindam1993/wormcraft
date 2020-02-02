3fd92eefddeba1652ebcf27b40cc4f1c1f2939da
arindam1993 <arindam.1993@gmail.com>
2020-02-02 12:52:21 -0800

    worm be respawning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,41 +9,42 @@ using System.Collections.Generic;
 //
 // See comments in PlayerManager.cs for more details.
 //
-public class Player : MonoBehaviour
+public class Player : BaseDisableable
 {
     public PlayerActions Actions { get; set; }
     public int PlayerIndex { get; set; }
 
     Color[] playerColors = { Color.green, Color.red, Color.blue, Color.yellow };
     public float forceMultiplier;
-
-    List<SpriteRenderer> cachedRenderers = new List<SpriteRenderer>();
+    
+    List<Vector2> initialRelativePositions = new List<Vector2>();
+    List<GameObject> childGameObjects = new List<GameObject>();
     WormEnd endA;
     WormEnd endB;
 
     public Bounds displayBounds { get; set; }
+    SpriteRenderer outlineA;
+    SpriteRenderer outlineB;
 
     void Start()
 	{
         int count = this.transform.childCount;
+        // track initial setup so we can reset state when enabling and siabling the worm
         for (int i=0; i < count; i++)
         {
             Transform child = this.transform.GetChild(i);
-            if (child.gameObject.name.Contains("Seg"))
-            {
-                SpriteRenderer currentRenderer = child.gameObject.GetComponent<SpriteRenderer>();
-                cachedRenderers.Add(currentRenderer);
-                //Color newColor = playerColors[PlayerIndex] * 0.3f;
-                //newColor.a = 1.0f;
-                //currentRenderer.color = newColor;
-            }
+
+            initialRelativePositions.Add(child.position - this.transform.position);
+            childGameObjects.Add(child.gameObject);
         }
         Color playerColor = playerColors[PlayerIndex];
         endA = this.transform.Find("End_A").GetComponent<WormEnd>();
-        endA.transform.Find("Outline").GetComponent<SpriteRenderer>().color = playerColor;
+        outlineA = endA.transform.Find("Outline").GetComponent<SpriteRenderer>();
+        outlineA.color = playerColor;
 
         endB = this.transform.Find("End_B").GetComponent<WormEnd>();
-        endB.transform.Find("Outline").GetComponent<SpriteRenderer>().color = playerColor;
+        outlineB = endB.transform.Find("Outline").GetComponent<SpriteRenderer>();
+        outlineB.color = playerColor;
 
         displayBounds = CalcDisplayBounds();
     }
@@ -51,29 +52,32 @@ public class Player : MonoBehaviour
 
 	void Update()
 	{
-        if (Actions.GrabA.IsPressed)
+        if (!IsWormcraftDisabled())
         {
-            endA.Release();
-        }
-        else
-        {
-            endA.Grab();
-        }
+            if (Actions.GrabA.IsPressed)
+            {
+                endA.Release();
+            }
+            else
+            {
+                endA.Grab();
+            }
 
-        if (Actions.GrabB.IsPressed)
-        {
-            endB.Release();
+            if (Actions.GrabB.IsPressed)
+            {
+                endB.Release();
+            }
+            else
+            {
+                endB.Grab();
+            }
+            WormEnd grabEnd = GetFreeEnd();
+            if (grabEnd != null)
+            {
+                grabEnd.ApplyForce(Actions.joystick.Value * forceMultiplier);
+            }
         }
-        else
-        {
-            endB.Grab();
-        }
-        WormEnd grabEnd = GetFreeEnd();
-        if(grabEnd != null)
-        {
-            grabEnd.ApplyForce(Actions.joystick.Value * forceMultiplier);
-        }
-
+       
         displayBounds = CalcDisplayBounds();
     }
 
@@ -100,7 +104,46 @@ public class Player : MonoBehaviour
         }
 
         return endA.IsGrabbingStatic() ? endB : endA;
-    } 
+    }
+
+    public override void WormcraftDisable()
+    {
+        base.WormcraftDisable();
+
+        for(int cIdx = 0; cIdx < childGameObjects.Count; cIdx ++) 
+        {
+            GameObject child = childGameObjects[cIdx];
+            child.GetComponent<Rigidbody2D>().simulated = false;
+            child.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        for (int cIdx = 0; cIdx < childGameObjects.Count; cIdx++)
+        {
+            GameObject child = childGameObjects[cIdx];
+            child.transform.position = new Vector2(this.transform.position.x, this.transform.position.y) + initialRelativePositions[cIdx];
+        }
+        outlineA.enabled = false;
+        outlineB.enabled = false;
+
+    }
+
+    public override void WormcraftEnable()
+    {
+        base.WormcraftEnable();
+        for (int cIdx = 0; cIdx < childGameObjects.Count; cIdx++)
+        {
+            GameObject child = childGameObjects[cIdx];
+            child.GetComponent<Rigidbody2D>().simulated = true;
+            child.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        outlineA.enabled = true;
+        outlineB.enabled = true;
+
+    }
+
+    public override Vector2 GetCenter()
+    {
+        return childGameObjects[childGameObjects.Count / 2].transform.position;
+    }
 }
 
 
---
+++ Assets/Scripts/Respawnable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawnable : MonoBehaviour
+{
+    public static float BottomY = -15.0f;
+    public static float TopY = 6.0f;
+
+    public float RespawnTime;
+    BaseDisableable disableTarget;
+    bool isRespawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        disableTarget = this.gameObject.GetComponent<BaseDisableable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 center = disableTarget.GetCenter();
+        if(center.y < BottomY)
+        {
+            if (isRespawning)
+            {
+                StartCoroutine(Respawn());
+            }
+            isRespawning = true;
+            
+        }
+    }
+
+
+    IEnumerator Respawn()
+    {
+        disableTarget.WormcraftDisable();
+        yield return new WaitForSeconds(RespawnTime);
+        this.transform.position = new Vector3(Camera.main.transform.position.x, TopY, 0);
+        disableTarget.WormcraftEnable();
+        isRespawning = false;
+    }
+}
